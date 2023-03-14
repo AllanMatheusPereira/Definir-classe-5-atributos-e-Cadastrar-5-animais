a2ea57370b83d674ff47409b9ffac9bd63b4d740
Allan Pereira <107968062+AllanMatheusPereira@users.noreply.github.com>
2023-03-14 20:50:52 -0300

    Add files via upload

---
+++ Cadastro de Animais/AnimaisdoAlan/src/main/java/exercicios/animaisdoalan/Animais.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicios.animaisdoalan;
+
+/**
+ *
+ * @author allan
+ */
+public class Animais {
+     String animal;
+     String tipo;
+     String porte;
+     String cor;
+     String habitat;
+     
+    
+}
+
+
